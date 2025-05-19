a93b7bb27a2a651c839813430c9e8aad2644c99a
Yoga Muhammad Fauzi <yogamfauzi@gmail.com>
2025-05-19 22:53:30 +0700

    Perbaiki vite.config.js dan atur output dist

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,10 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-	    outDir: 'dist'
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'dist', // output folder untuk hasil build
+    },
 });
